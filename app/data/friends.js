3d363b78ed279acea1213d08c81683bb64206abb
Brandon White <46477427+iambrandonwhite@users.noreply.github.com>
2019-04-27 23:23:16 -0700

    updated friends

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendProfile = [
+	{
+		name: 'Jeff Lebowski',
+		photo: 'https://gq-images.condecdn.net/image/N25V1omb0Qa/crop/405/landscape/f/The-Big-Lebowski-hp-GQ-25Feb16_rex_b.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Tony Montana',
+		photo: 'https://i.pinimg.com/originals/f2/c2/3b/f2c23b8e54df9430c581fa4bff8910b5.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Leatherface',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/0/09/Leatherfaceentertainmentweekly.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Hunter S Thompson',
+		photo: 'https://www.thefamouspeople.com/profiles/images/hunter-s-thompson-4.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Batman',
+		photo: 'https://i.pinimg.com/originals/5d/ba/5d/5dba5dfee1836fc4da067d02bd1a0449.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Tom Hanks',
+		photo: 'http://1ryzas42x65e2oosia40bgli.wpengine.netdna-cdn.com/wp-content/uploads/2017/10/2017-10-25-1-1.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Darth Vader',
+		photo: 'https://cdn.shopify.com/s/files/1/0558/2081/products/Mitchell_DarthVader_FINAL_sm_1024x1024.jpg?v=1493912085',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Freddy Kruger',
+		photo: 'https://i.pinimg.com/originals/a8/ae/9f/a8ae9f35d66d7d5fdf09611185c7e4da.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jason',
+		photo: 'https://vignette.wikia.nocookie.net/nightmareonelmstreet/images/6/68/Jason_Voorhees_FvJ.jpg/revision/latest?cb=20171006105801',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Steve Buschemi',
+		photo: 'https://shawetcanada.files.wordpress.com/2016/06/2226bf7a5aca105cdf0e8c59d6f7190e.jpg?quality=80&strip=all&w=605',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendProfile;
