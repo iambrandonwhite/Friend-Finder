e0b127c4800f8af473007f30deed2634b527b704
Brandon White <46477427+iambrandonwhite@users.noreply.github.com>
2019-04-27 22:27:17 -0700

    Add files via upload

---
+++ htmlRoutes.js
@@ -0,0 +1,16 @@
+//Contains all html routes
+var express = require("express");
+var path = require("path");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+ console.log(__dirname);
+ res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+router.get("/survey", function (req, res) {
+ res.sendFile(path.join(__dirname, "../public/survey.html"));
+})
+
+module.exports = router;
